4113bb418f944df0ee2ac7d7f69b18dc80463328
Jean C. Mendoza <73866099+MenJCP@users.noreply.github.com>
2021-09-24 13:00:31 -0400

    Added the program

---
+++ AGOOCT2021_CS-master/CS.WebApplication/Controllers/CalculatorController.cs
@@ -0,0 +1,51 @@
+﻿using CS.WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS.WebApplication.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(OperationModel model)
+        {
+            decimal Result = 0;
+
+            if(model.OperationSymbol == "+")
+            {
+                Result = model.Param1 + model.Param2;
+            }
+            else if(model.OperationSymbol == "-")
+            {
+                Result = model.Param1 - model.Param2;
+            }
+            else if (model.OperationSymbol == "*")
+            {
+                Result = model.Param1 * model.Param2;
+            }
+            else if (model.OperationSymbol == "/")
+            {
+                Result = model.Param1 / model.Param2;
+            }
+            else
+            {
+                Result = -1;
+            }
+
+            model.Result = $"El resultado es {model.Param1} {model.OperationSymbol} {model.Param2} = {Result}";
+
+                return View(model);
+        }
+
+
+    }
+}
---
+++ AGOOCT2021_CS-master/CS.WebApplication/Models/OperationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS.WebApplication.Models
+{
+    public class OperationModel
+    {
+        public decimal Param1 { get; set; }
+        public decimal Param2 { get; set; }
+        public string OperationSymbol { get; set; }
+        public string Result { get; set; }
+
+    }
+}
